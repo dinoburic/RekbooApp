688ad0e7d0292d5ddc91bd36eba08003b690029c
Dino Burić <165225286+nodib202@users.noreply.github.com>
2024-08-27 02:16:01 +0200

    Created MealItem component

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,28 +1,29 @@
+import { useEffect } from "react";
 import { useState } from "react";
+import MealItem from "./MealItem";
 
 
 export default  function Meals()  {
 
    const [loadedMeals, setLoadedMeals] = useState([]);
 
-   async function fetchMeals() {
-    const response = await fetch('http://localhost:3000/meals');
-    
-    if(!response.ok) {
-
-    }
-
-    const meals = await response.json();
-    setLoadedMeals(meals);
-}
-    
-fetchMeals();
+    useEffect(()=>{
+        async function fetchMeals() {
+            const response = await fetch('http://localhost:3000/meals');
+            
+            if(!response.ok) {
+        
+            }
+        
+            const meals = await response.json();
+            setLoadedMeals(meals);
+        }
+    },[]);
 
     return(
         <ul id="meals">
-            {loadedMeals.map(meal => <li id={meal.id}>
-                {meal.name}
-            </li>)}
+            {loadedMeals.map((meal) =>
+            <MealItem key={meal.id} meal={meal}/> )}
         </ul>
     );
 }

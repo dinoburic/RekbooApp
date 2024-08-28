6908e45ad938d5172eff315e0a95f0b49784a167
Dino Burić <165225286+nodib202@users.noreply.github.com>
2024-08-29 01:30:21 +0200

    Created Cart Modal popup

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,19 +2,27 @@ import { useContext } from 'react';
 import logo from '../assets/logo.png'
 import Button from './UI/Button.jsx';
 import CartContext from '../store/CartContext.jsx';
+import UserProgressContext from '../store/UserProgressContext.jsx';
 
 export default function Header() {
    const cartCtx= useContext(CartContext);
+   const userProgressCtx = useContext(UserProgressContext);
+   
    const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item)=>{
     return totalNumberOfItems+item.quantity;
    },0);
-    return(
+
+   function handleShowCart() {
+    userProgressCtx.showCart();
+   }
+    
+   return(
         <header id="main-header">
             <div id="title">
                 <img src={logo} alt='Rekboo logo' />
             </div>
             <nav>
-                <Button textOnly>Cart ({totalCartItems})</Button>
+                <Button onClick={handleShowCart} textOnly>Cart ({totalCartItems})</Button>
             </nav>
         </header>
     );

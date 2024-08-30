26c506d0aba3dbec76d9d145aaf3fd44b24c7be6
Dino Burić <165225286+nodib202@users.noreply.github.com>
2024-08-30 22:27:57 +0200

    Implemented  Checkout feature

---
+++ src/components/Checkout.jsx
@@ -0,0 +1,39 @@
+import { useContext } from "react";
+import Modal from "./UI/Modal";
+import CartContext from "../store/CartContext";
+import { currencyFormatter } from "../util/formatting";
+import Input from "./Input";
+import Button from "./UI/Button";
+import UserProgressContext from "../store/UserProgressContext";
+
+export default function Checkout() {
+    const cartCtx=useContext(CartContext);
+    const userProgressCtx=useContext(UserProgressContext);
+    
+    const cartTotal = cartCtx.items.reduce((totalPrice,item)=>totalPrice+item.quantity*item.price,0)
+    
+    function handleClose() {
+        userProgressCtx.hideCheckout();
+    }
+    
+    return(
+        <Modal onClose={handleClose} open={userProgressCtx.progress==='checkout'}>
+            <form>
+                <h2>Checkout</h2>
+                <p>Total Amount: {currencyFormatter.format(cartTotal)} </p>
+            </form>
+
+            <Input label="Full Name" type="text" id="full-name"/>
+            <Input label="Email Address" type="email" id="email"/>
+            <Input label="Street" type="text" id="street"/>
+            <div className="control-row">
+            <Input label="Postal Code" type="text" id="postal-code"/>
+            <Input label="City" type="text" id="city"/>
+            </div>
+            <p className="modal-actions">
+                <Button type="button" onClick={handleClose} textOnly>Close</Button>
+                <Button>Submit Order</Button>
+            </p>
+        </Modal>
+    );
+}
--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -35,12 +35,12 @@ function cartReducer(state, action) {
 
         const existingCartItem = state.items[existingCartItemIndex];
 
-        const updatedItems = [...state.item];
+        const updatedItems = [...state.items];
 
         if(existingCartItem.quantity === 1) {
             updatedItems.splice(existingCartItemIndex,1);
         } else {
-            const updatedItem    = {
+            const updatedItem = {
                 ...existingCartItem,
                 quantity: existingCartItem.quantity -1
             }
@@ -58,8 +58,6 @@ function cartReducer(state, action) {
 
 export function CartContextProvider({ children }) {
     const [cart, dispatchCartAction]=useReducer(cartReducer, { items: [] });
-
-  
 
     function addItem(item) {
         dispatchCartAction({
